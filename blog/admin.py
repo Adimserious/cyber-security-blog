781edc9c870f798b874e63c754e4e33f746b5379
Adimserious Amatobi <lilianadimchinobi@gmail.com>
2024-06-10 11:05:00 +0000

    Add status to post model

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Blog_posts
+from .models import Blog_post
 
 # Register your models here.
-admin.site.register(Blog_posts)
+admin.site.register(Blog_post)
--- blog/models.py
+++ blog/models.py
@@ -3,15 +3,17 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 
 
+STATUS_CHOICES = ((0, 'pending'), (1, "done"))
 # Create your models here.
-class Blog_posts(models.Model):
+class Blog_post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_post")
     title = models.CharField(max_length=250, unique=True)
     slug = models.SlugField(max_length=250, unique=True)
     body_content = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
     created = models.DateTimeField(auto_now_add=True)
     published = models.DateTimeField(default=timezone.now)
     updated = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS_CHOICES, default=0)
     # Meta data class with ordering attribute to sort result by the published field
     class Meta:
         ordering = ["-published"]
